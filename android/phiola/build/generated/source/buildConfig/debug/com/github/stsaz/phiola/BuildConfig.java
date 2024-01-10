6c5714d0d6100cc9f03d2d0108f3c4b832eae81d
neoe <hi2@n101n.xyz>
2024-01-10 20:56:46 +0900

    recover from bad merge

---
+++ android/phiola/build/generated/source/buildConfig/debug/com/github/stsaz/phiola/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.stsaz.phiola;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.github.stsaz.phiola.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 20015;
+  public static final String VERSION_NAME = "2.0-beta15";
+}
